3615dc1f874b463914ae2cfaa8e1273ed5acd7cf
Vale-JP <dmoreiravale@gmail.com>
2024-06-17 13:56:05 -0300

    Solução dos Exercícios

--- exercises/Sistema de Gestão de Pedidos de Restaurante/Restaurante.js
+++ exercises/Sistema de Gestão de Pedidos de Restaurante/Restaurante.js
@@ -5,7 +5,21 @@ class Restaurante {
         this.pedidos = [];
     }
 
-   
+    adicionarPedido(pedido) {
+        this.pedidos.push(pedido);
+    }
+
+    removerPedido(numeroPedido) {
+        this.pedidos = this.pedidos.filter(pedido => pedido.numero !== numeroPedido);
+    }
+
+    listarPedidosPendentes() {
+        return this.pedidos.filter(pedido => !pedido.pronto);
+    }
+
+    listarPedidosProntos() {
+        return this.pedidos.filter(pedido => pedido.pronto);
+    }
 }
 
 module.exports = Restaurante;
--- exercises/Sistema de Loja Online com Carrinho de Compras/Carrinho.js
+++ exercises/Sistema de Loja Online com Carrinho de Compras/Carrinho.js
@@ -4,10 +4,24 @@ class Carrinho {
     }
 
     adicionarItem(produto, quantidade) {
-        
+       this.itens.push({produto, quantidade});
     }
 
-    
+    removerItem(produto) {
+        this.itens = this.itens.filter(item => item.produto !== produto);
+    }
+
+    calcularTotal() {
+        let total = 0;
+        this.itens.forEach(item => {
+            total += item.produto.preco * item.quantidade;
+        });
+        return total;
+    }
+
+    listarItens() {
+        return this.itens;
+    }
 }
 
 module.exports = Carrinho;
