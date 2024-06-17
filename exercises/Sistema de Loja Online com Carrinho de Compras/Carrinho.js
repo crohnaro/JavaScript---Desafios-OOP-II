54f1e2f38c22ec9896c3af0015976340e6cc36ff
augusto <augustosenac2@gmail.com.br>
2024-06-17 15:46:11 -0300

    Solução dos exercícios

--- exercises/Sistema de Loja Online com Carrinho de Compras/Carrinho.js
+++ exercises/Sistema de Loja Online com Carrinho de Compras/Carrinho.js
@@ -4,10 +4,20 @@ class Carrinho {
     }
 
     adicionarItem(produto, quantidade) {
-        
+        this.itens.push({ produto, quantidade });
     }
 
-    
+    removerItem(produto) {
+        this.itens = this.itens.filter(item => item.produto !== produto);
+    }
+
+    calcularTotal() {
+        return this.itens.reduce((total, item) => total + item.produto.preco * item.quantidade, 0);
+    }
+
+    listarItens() {
+        return this.itens;
+    }
 }
 
 module.exports = Carrinho;
