915b4d0464a9162fe931cae7206977894c23023a
aErica26 <ericanepomuceno43@gmail.com>
2024-06-18 14:17:50 -0300

    Solução dos exercícios

--- exercises/Sistema de Gestão de Pedidos de Restaurante/Restaurante.js
+++ exercises/Sistema de Gestão de Pedidos de Restaurante/Restaurante.js
@@ -5,7 +5,21 @@ class Restaurante {
         this.pedidos = [];
     }
 
-   
+    adicionarPedido(pedido) {
+        this.pedidos.push(pedido);
+    }
+
+    removerPedido(numero) {
+        this.pedidos = this.pedidos.filter(pedido => pedido.numero !== numero);
+    }
+
+    listarPedidosPendentes() {
+        return this.pedidos.filter(pedido => !pedido.pronto);
+    }
+
+    listarPedidosProntos() {
+        return this.pedidos.filter(pedido => pedido.pronto);
+    }
 }
 
 module.exports = Restaurante;
